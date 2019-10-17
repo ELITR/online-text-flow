54887f732e389d36ea45b8c023c4a23a18c7fbfe
Otakar Smrz <otakar-smrz@users.sf.net>
2019-10-17 05:37:47 +0200

    Evolve the data flow

--- online-text-flow-client.py
+++ online-text-flow-client.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 
 import requests
+import json
 import sys
 
-def main(kind='', url='http://127.0.0.1:5000'):
+def main(url='http://127.0.0.1:5000'):
     for line in sys.stdin:
         try:
-            line = " ".join(line.split())
-            (f, s, t) = line.split(None, 2)
-            data = (int(f), int(s), t)
+            event = json.loads(line)
         except:
             print(line)
         else:
-            r = requests.post(url + '/post', json={"data": data, "kind": kind})
-            print(r.text)
+            reply = requests.post(url + '/post', json=event)
+            print(reply.text)
 
 if __name__ == '__main__':
     main(*sys.argv[1:])
--- online-text-flow-server.py
+++ online-text-flow-server.py
@@ -3,132 +3,38 @@
 from flask import json
 
 import flask
-import datetime
 import time as sleep
-import re
 
 app = flask.Flask(__name__, template_folder=".")
 
-DATA = []
-FLOW = {}
+DATA = {'id': ''}
 
 
-def eventsData():
-    where = 0
+def events():
+    uniq = ''
     while True:
-        if len(DATA) > where:
-            kind = DATA[where]['kind']
-            if kind:
-                show = 'event: %s\n' % kind
-                uniq = 'data-%s-%d' % (kind, where + 1)
-            else:
-                show = ''
-                uniq = 'data-%d' % (where + 1)
-            data = json.dumps(DATA[where])
+        if not uniq == DATA['id']:
+            show = 'event: %s\n' % DATA['event'] if 'event' in DATA else ''
+            uniq = DATA['id']
+            data = json.dumps(DATA['data'])
             yield '%sid: %s\ndata: %s\n\n' % (show, uniq, data)
-            where += 1
-        else:
-            sleep.sleep(1)
-
-
-def eventsFlow():
-    where = {}
-    while True:
-        for kind in FLOW:
-            if kind not in where:
-                where[kind] = 0
-            if len(FLOW[kind]) > where[kind]:
-                if kind:
-                    show = 'event: %s\n' % kind
-                    uniq = 'flow-%s-%d' % (kind, where[kind] + 1)
-                else:
-                    show = ''
-                    uniq = 'flow-%d' % (where[kind] + 1)
-                data = FLOW[kind][where[kind]]
-                data = ",\ndata:  ".join([ json.dumps(line) for line in data ])
-                yield '%sid: %s\ndata: [%s]\n\n' % (show, uniq, data)
-                where[kind] += 1
-
-
-def updateData(data):
-    DATA.append(data)
-
-
-def updateFlow(data):
-    kind = data['kind']
-    line = data['data']
-    if kind not in FLOW:
-        FLOW[kind] = [[line]]
-    else:
-        flow = FLOW[kind][-1]
-        f = len(flow) - 1
-        for f in range(f, -1, -1):
-            if flow[f][0] < line[0]:
-                f += 1
-                break
-        t = f
-        for t in range(t, len(flow)):
-            if flow[t][0] >= line[1]:
-                t -= 1
-                break
-        flow = flow[:f] + [line] + flow[t + 1:]
-        FLOW[kind].append(flow)
-
-
-@app.route('/stat')
-def stat():
-    if len(DATA):
-        return "Date: %s\nTime: %s\nSize: %s\n" % (DATA[-1]['date'], DATA[-1]['time'], len(DATA))
-    else:
-        return ""
-
-
-@app.route('/dump')
-def dump():
-    return json.jsonify({'DATA': DATA, 'FLOW': FLOW})
-
-
-@app.route('/remove/<kind>')
-def clear(kind):
-    global DATA
-    size = len(DATA)
-    DATA = [ line for line in DATA if line['kind'] != kind ]
-    del FLOW[kind]
-    return json.jsonify({'kind': kind, 'size': len(DATA) - size})
 
 
 @app.route('/post', methods=['POST'])
 def post():
-    data = flask.request.json
-    user = flask.session.get('user', 'anonymous')
-    when = datetime.datetime.now().replace(microsecond=0)
-    time = when.time()
-    date = when.date()
-    kind = " ".join(data['kind'].split())
-    kind = re.sub('\W', '-', kind)
-    updateData({"time": time.isoformat(),
-                "date": date.isoformat(),
-                "user": user,
-                "data": data['data'],
-                "kind": kind})
-    updateFlow(DATA[-1])
-    return json.jsonify(DATA[-1])
+    global DATA
+    DATA = flask.request.json
+    return json.jsonify(DATA)
 
 
 @app.route('/data')
 def data():
-    return flask.Response(eventsData(), mimetype="text/event-stream")
-
-
-@app.route('/flow')
-def flow():
-    return flask.Response(eventsFlow(), mimetype="text/event-stream")
+    return flask.Response(events(), mimetype="text/event-stream")
 
 
 @app.route('/')
 def home():
-    (en, cs) = [ "no events in the '%s' stream yet" % k for k in ["en", "cs"] ]
-    return flask.render_template('index.html', text={"en": en, "cs": cs})
+    return flask.render_template('index.html', data='/data')
 
 
 if __name__ == '__main__':
