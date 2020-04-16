4f8b83fe2eff10f00bd7c26eac028001d6b4cc6c
Dominik Macháček <gldkslfmsd@gmail.com>
2020-04-16 20:42:23 +0200

    forgotten to_brief

---
+++ elitr/onlinetextflow/to_brief.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import click
+
+try:
+    from .textflow_protocol import *
+except ImportError:
+    from elitr.onlinetextflow.textflow_protocol import *
+
+@click.command(context_settings={'help_option_names': ['-h', '--help']})
+def main():
+    '''Converts into the brief text-flow.'''
+    import sys
+    for line in original_to_brief(sys.stdin):
+        print(line, flush=True, end="")
+
+if __name__ == "__main__":
+    main()
+
