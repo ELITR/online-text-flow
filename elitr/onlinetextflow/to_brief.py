42444742b477722b3f03df01f3c75ab8aa17018a
Dominik Macháček <machacek@ufal.mff.cuni.cz>
2021-03-11 18:11:01 +0100

    to_brief/from_brief was swapped

--- elitr/onlinetextflow/to_brief.py
+++ elitr/onlinetextflow/to_brief.py
@@ -2,16 +2,13 @@
 
 import click
 
-try:
-    from .textflow_protocol import *
-except ImportError:
-    from elitr.onlinetextflow.textflow_protocol import *
+from elitr.onlinetextflow.textflow_protocol import *
 
 @click.command(context_settings={'help_option_names': ['-h', '--help']})
 def main():
-    '''Converts into the brief text flow from the original one.'''
+    '''Converts from the brief text flow into the original one.'''
     import sys
-    for line in brief_to_original(sys.stdin):
+    for line in original_to_brief(sys.stdin):
         print(line, flush=True, end="")
 
 if __name__ == "__main__":
