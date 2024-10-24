ec134bcd2bf25483a0826888547636e3bb625efc
Dominik Machacek <machacek@sv-1.ufal.mff.cuni.cz>
2024-10-24 18:20:39 +0200

    config changes

--- elitr/onlinetextflow/server/config.py
+++ elitr/onlinetextflow/server/config.py
@@ -6,15 +6,22 @@ host = '127.0.0.1'
 
 auth = {'user': 'elitr', 'pass': 'elitr'}
 
-menu = ['en', 'cs', 'ar', 'az', 'be', 'bg',
-        'bs', 'da', 'de', 'el', 'es', 'et',
-        'fi', 'fr', 'ga', 'he', 'hr', 'hu',
-        'hy', 'is', 'it', 'ka', 'kk', 'lb',
-        'lt', 'lv', 'me', 'mk', 'mt', 'nl',
-        'no', 'pl', 'pt', 'ro', 'ru', 'sk',
-        'sl', 'sq', 'sr', 'sv', 'tr', 'uk']
+#menu = ['en', 'asr', 'cs', 'ar', 'az', 'be', 'bg',
+#        'bs', 'da', 'de', 'el', 'es', 'et',
+#        'fi', 'fr', 'ga', 'he', 'hr', 'hu',
+#        'hy', 'is', 'it', 'ka', 'kk', 'lb',
+#        'lt', 'lv', 'me', 'mk', 'mt', 'nl',
+#        'no', 'pl', 'pt', 'ro', 'ru', 'sk',
+#        'sl', 'sq', 'sr', 'sv', 'tr', 'uk',
+#        'cs']
 
-show = ['en', 'de', 'cs']
+#show = "en cs".split()
+show = "en cs".split()
+
+
+all_menu = "ar az be bg bs cs ca da de el es et fi fr ga he hr hu hy is it ka kk lb lt lv me mk mt nb nl nn pl pt ro ru sk sl sq sr_cyrillic sr_latin sv tr uk".split()
+
+menu = show + [a for a in all_menu if a not in show] + ["asr", "zh"]
 
 hide = []
 
